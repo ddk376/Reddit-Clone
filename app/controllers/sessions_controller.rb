5e924a2e77bbf8f6aaf76748273de2dfd9d06b78
gina <ddk376@gmail.com>
2016-05-26 15:37:44 -0400

    Code Formating

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    @user = User.find_by_credentials(
+                    params[:user][:username],
+                    params[:user][:password]
+                  )
     if !!@user
       log_in_user!(@user)
       redirect_to user_url(@user)
